368081683a7e200a6baa160342373339180382db
Hessam Imen <hessam.imen@gmail.com>
2022-03-23 20:43:19 +1100

    Done with assignemnt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const taskForm = document.getElementById('create-task-form');
+
+
+taskForm.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  const newTask = document.getElementById('new-task-description');
+
+  if (newTask.value !== ''){
+    //variables
+  const taskList = document.getElementById('tasks');
+  const taskItem = document.createElement('li')
+  const spanText = document.createElement('span')
+  spanText.innerHTML = newTask.value;
+  
+  taskItem.appendChild(spanText)
+  taskList.appendChild(taskItem);
+
+//Delete button
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = 'X'
+  deleteBtn.style.marginLeft = '10px';
+  deleteBtn.style.paddingTop = '3px';
+  deleteBtn.style.color = 'red';
+  taskItem.appendChild(deleteBtn);
+  deleteBtn.addEventListener('click', ()=>{
+    taskItem.remove()
+  })
+
+//Edit button
+  const editBtn = document.createElement('button');
+  editBtn.innerHTML = 'Edit';
+  editBtn.style.marginLeft = '2px';
+  editBtn.style.paddingTop = '3px';
+  editBtn.style.color = 'green';
+  taskItem.appendChild(editBtn);
+  editBtn.addEventListener('click', ()=>{
+  spanText.innerHTML = newTask.value;
+  taskForm.reset();
+  })
+  }
+  taskForm.reset();
 });
+});
+
+
+
